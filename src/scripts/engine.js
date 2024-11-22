6f8b843b2b8811c48438e458f488b76d9acdcd77
Ulisses <ulissesmkt2104@gmail.com>
2024-11-22 18:58:26 -0300

    Atualização: Adicionando a função que dá funcionalidade ao toggle e tira as letras das teclas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,8 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input")
+const keysCheck = document.querySelector(".keys-check input")
+
+
 let mapedKeys = []
 let audio = new Audio("src/tunes/a.wav")
 
@@ -30,4 +33,10 @@ const handleVolume = (e) => {
     audio.volume = e.target.value
 }
 
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
 volumeSlider.addEventListener("input",handleVolume)
+
+keysCheck.addEventListener("click", showHideKeys)
